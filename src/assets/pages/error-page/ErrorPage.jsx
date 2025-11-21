431fcd1cc89b775739282326de9bd52aa3f6ffb5
suzegeerdink <geerdinksuze@gmail.com>
2025-11-21 15:48:59 +0100

    setup added basic routing

--- src/assets/pages/error-page/ErrorPage.jsx
+++ src/assets/pages/error-page/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import './ErrorPage.css'
+import {Link} from "react-router-dom";
 
 function ErrorPage() {
     return (
         <>
             <p>ErrorPage</p>
+            <p>Take me back to the <Link to="/">homepage!</Link></p>
         </>
     )
 }
